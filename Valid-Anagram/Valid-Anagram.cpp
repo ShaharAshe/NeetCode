6b6ed4992f1cc9d9b98eb744b9c441f34a94d15f
ShaharAshe <shaharas30@gmail.com>
2026-03-30 19:03:51 +0300

    Valid Anagram

--- Valid-Anagram/Valid-Anagram.cpp
+++ Valid-Anagram/Valid-Anagram.cpp
@@ -3,26 +3,20 @@
 #include <string>
 #include <unordered_map>
 
-class Solution
-{
+class Solution {
 public:
-    bool isAnagram(std::string s, std::string t)
-    {
-        if (s.size() != t.size())
-        {
+    bool isAnagram(std::string s, std::string t) {
+        if (s.size() != t.size()) {
             return false;
         }
         std::unordered_map<char, int> s_letters_existens_map;
-        for (size_t i = 0; i < s.size(); ++i)
-        {
+        for (size_t i = 0; i < s.size(); ++i) {
             ++s_letters_existens_map[s[i]];
             --s_letters_existens_map[t[i]];
         }
 
-        for (auto &cell : s_letters_existens_map)
-        {
-            if (0 < cell.second || cell.second < 0)
-            {
+        for (auto &cell : s_letters_existens_map) {
+            if (0 < cell.second || cell.second < 0) {
                 return false;
             }
         }
@@ -30,8 +24,7 @@ public:
     }
 };
 
-int main()
-{
+int main() {
     std::string s = "racecar",
                 t = "carrace";
     auto result = Solution().isAnagram(s, t);
