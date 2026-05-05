a96141357c1393de96072d7d04641bbd46934d75
ShaharAshe <shaharas30@gmail.com>
2026-05-05 16:24:01 +0300

    3Sum

---
+++ 3Sum/3Sum.cpp
@@ -0,0 +1,54 @@
+#include <cstdlib>
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+class Solution {
+public:
+    std::vector<std::vector<int>> threeSum(std::vector<int>& nums) {
+        std::sort(nums.begin(), nums.end());
+        std::vector<std::vector<int>> result;
+        
+        for (int i = 0; i < nums.size(); ++i) {
+            if (1 <= i && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left_i = i + 1,
+                right_i = nums.size() - 1;
+            
+            while (left_i < nums.size() && left_i < right_i) {
+                int sum = nums[i] + nums[left_i] + nums[right_i];
+                if (sum == 0) {
+                    std::vector<int> temp_vector{nums[i], nums[left_i], nums[right_i]};
+                    result.emplace_back(temp_vector);
+                    ++left_i;
+                    while (nums[left_i] == nums[left_i - 1]) {
+                        ++left_i;
+                    }
+                }
+                if (sum < 0) {
+                    ++left_i;
+                } else {
+                    --right_i;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+int main() {
+    std::vector<int> nums{-1,0,1,2,-1,-4};
+    // std::vector<int> nums{0,0,0,0};
+
+    auto result = Solution().threeSum(nums);
+
+    for (auto &arr : result) {
+        for (auto &c : arr) {
+            std::cout << c << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return EXIT_SUCCESS;
+}
